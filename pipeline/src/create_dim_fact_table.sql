2560e6b93ea13fc6ff52d454aaced95ee1492ebd
Luissalazarsalinas <lf.salazarsalinas@gmail.com>
2023-09-22 11:05:52 -0500

    pipeline

---
+++ pipeline/src/create_dim_fact_table.sql
@@ -0,0 +1,21 @@
+-- Create dimentionals and fact tables 
+
+CREATE TABLE Dim_crops(
+
+);
+
+CREATE TABLE Dim_location(
+
+);
+
+CREATE TABLE Dim_soils(
+
+);
+
+CREATE TABLE Dim_date(
+
+);
+
+CREATE TABLE Fact_crops(
+
+);
