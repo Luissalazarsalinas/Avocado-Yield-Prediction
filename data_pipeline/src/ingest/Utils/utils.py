d51625240f2e513c2e40c7020a592b67cf37a656
Luissalazarsalinas <lf.salazarsalinas@gmail.com>
2023-10-06 19:13:45 -0500

    urls

--- data_pipeline/src/ingest/Utils/utils.py
+++ data_pipeline/src/ingest/Utils/utils.py
@@ -3,8 +3,9 @@
 urls = [
     "https://www.datos.gov.co/api/views/2pnw-mmge/rows.csv?accessType=DOWNLOAD",
     "https://www.datos.gov.co/api/views/ch4u-f3i5/rows.csv?accessType=DOWNLOAD"
-    #"https://www.datos.gov.co/api/views/sbwg-7ju4/rows.csv?query=select%20*%20where%20%60fechaobservacion%60%20%3E%3D%20%272006-01-01%27%20AND%20%60fechaobservacion%60%20%3C%20%272019-01-02%27&read_from_nbe=true&version=2.1&accessType=DOWNLOAD",
-    #"https://www.datos.gov.co/api/views/ch4u-f3i5/rows.csv?accessType=DOWNLOAD",
+    #"https://www.datos.gov.co/api/views/sbwg-7ju4/rows.csv?query=select%20*%20where%20%60fechaobservacion%60%20%3E%3D%20%272006-01-01%27%20AND%20%60fechaobservacion%60%20%3C%20%272011-01-01%27&read_from_nbe=true&version=2.1&accessType=DOWNLOAD",
+    #"https://www.datos.gov.co/api/views/sbwg-7ju4/rows.csv?query=select%20*%20where%20%60fechaobservacion%60%20%3E%3D%20%272011-01-01%27%20AND%20%60fechaobservacion%60%20%3C%20%272016-01-01%27&read_from_nbe=true&version=2.1&accessType=DOWNLOAD",
+    #"https://www.datos.gov.co/api/views/sbwg-7ju4/rows.csv?query=select%20*%20where%20%60fechaobservacion%60%20%3E%3D%20%272016-01-01%27%20AND%20%60fechaobservacion%60%20%3C%20%272019-01-01%27&read_from_nbe=true&version=2.1&accessType=DOWNLOAD",
     #"https://www.datos.gov.co/api/views/s54a-sgyg/rows.csv?query=select%20*%20where%20%60fechaobservacion%60%20%3E%3D%20%272006-01-01%27%20AND%20%60fechaobservacion%60%20%3C%20%272008-01-01%27&read_from_nbe=true&version=2.1&accessType=DOWNLOAD",
     #"https://www.datos.gov.co/api/views/s54a-sgyg/rows.csv?query=select%20*%20where%20%60fechaobservacion%60%20%3E%3D%20%272008-01-01%27%20AND%20%60fechaobservacion%60%20%3C%20%272010-01-01%27&read_from_nbe=true&version=2.1&accessType=DOWNLOAD",
     #"https://www.datos.gov.co/api/views/s54a-sgyg/rows.csv?query=select%20*%20where%20%60fechaobservacion%60%20%3E%3D%20%272010-01-01%27%20AND%20%60fechaobservacion%60%20%3C%20%272012-01-01%27&read_from_nbe=true&version=2.1&accessType=DOWNLOAD",
